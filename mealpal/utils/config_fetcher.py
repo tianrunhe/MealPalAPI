b6b344eed196b8f844e47dd43e7add60f1fe12da
Bruce Tian <tianrunhe@gmail.com>
2019-11-23 16:19:43 -0800

    Bug fix: configuration stored for the KMS should be the encrypted password, not the KMS ARN

--- mealpal/utils/config_fetcher.py
+++ mealpal/utils/config_fetcher.py
@@ -1,14 +1,15 @@
 import configparser
 import glob
+import os
 
 
 class ConfigFetcher(object):
-    def __init__(self, config_path='./config/*'):
+    def __init__(self, config_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../config/*')):
         self.config = configparser.ConfigParser()
         self.config.read(glob.glob(config_path))
 
     def get_account_info(self):
         return {
             "email": self.config.get("account", "email"),
-            "passwordKMSARN": self.config.get("account", "passwordKMSARN")
+            "encryptedPassword": self.config.get("account", "encryptedPassword")
         }
--- tests/mealpal/utils/conftest.py
+++ tests/mealpal/utils/conftest.py
@@ -1,9 +1,7 @@
 import pytest
-import os
 from mealpal.utils.config_fetcher import ConfigFetcher
 
 
 @pytest.fixture(autouse=True)
-def config_fetcher(request):
-    config_folder = os.path.join(request.fspath.dirname, '../../../', 'config/*')
-    return ConfigFetcher(config_folder)
+def config_fetcher():
+    return ConfigFetcher()
