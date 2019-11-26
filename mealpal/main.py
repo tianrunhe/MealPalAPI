7aac575d5ea31e8f786741411427016975f92500
Bruce Tian <tianrunhe@gmail.com>
2019-11-25 20:58:21 -0800

    MVP for reservation API

--- mealpal/main.py
+++ mealpal/main.py
@@ -1,6 +1,9 @@
 import requests
+import json
 from flask import Flask
 from flask import jsonify
+
+from mealpal.utils.logging_in_manager import LoggingInManager
 
 app = Flask(__name__)
 
@@ -18,3 +21,17 @@ def get_cities():
         for city in response.json()['result']
     ]
     return jsonify(data)
+
+
+@app.route('/reserve/<schedule_id>', methods=['GET', 'POST'])
+def user(schedule_id):
+    with LoggingInManager() as context:
+        reserve_data = {
+            'quantity': 1,
+            'schedule_id': schedule_id,
+            'pickup_time': '12:00pm-12:15pm',
+            'source': 'Web',
+        }
+        response = requests.post('https://secure.mealpal.com/api/v2/reservations', data=json.dumps(reserve_data),
+                                 headers=LoggingInManager.HEADERS, cookies=context.cookies)
+        return json.dumps({'success': response.ok}), response.status_code, {'ContentType': 'application/json'}
