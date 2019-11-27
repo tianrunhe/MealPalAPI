498b671d10cadaf09de3288e28388c0422d0e472
Bruce Tian <tianrunhe@gmail.com>
2019-11-26 20:34:02 -0800

    Add unit tests for reserve API

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+from unittest.mock import Mock
 from mealpal.main import app
 
 
@@ -13,3 +15,21 @@ def test_get_cities():
 
     assert response.status_code == 200
     assert b'Seattle' in response.data
+
+
+def test_reserve():
+    with patch('mealpal.main.requests') as patch_requests:
+        mocked_response = Mock()
+        patch_requests.post.return_value = mocked_response
+
+        mocked_response.ok = True
+        mocked_response.status_code = 200
+        response = app.test_client().post('/reserve/1234')
+        assert response.status_code == 200
+        assert b'{\"success\": true}' in response.data
+
+        mocked_response.ok = False
+        mocked_response.status_code = 500
+        response = app.test_client().post('/reserve/1234')
+        assert response.status_code == 500
+        assert b'{\"success\": false}' in response.data
