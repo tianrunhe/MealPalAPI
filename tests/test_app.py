62e7f05bc972f20edc7deaa7298267edef20f63e
Bruce Tian <tianrunhe@gmail.com>
2019-11-26 20:58:51 -0800

    Fix unit tests for reserve API

--- tests/test_app.py
+++ tests/test_app.py
@@ -21,15 +21,17 @@ def test_reserve():
     with patch('mealpal.main.requests') as patch_requests:
         mocked_response = Mock()
         patch_requests.post.return_value = mocked_response
+        with patch('mealpal.utils.logging_in_manager.decrypt') as patch_decrypt:
+            patch_decrypt.return_value = "MealPalAPITest"
 
-        mocked_response.ok = True
-        mocked_response.status_code = 200
-        response = app.test_client().post('/reserve/1234')
-        assert response.status_code == 200
-        assert b'{\"success\": true}' in response.data
+            mocked_response.ok = True
+            mocked_response.status_code = 200
+            response = app.test_client().post('/reserve/1234')
+            assert response.status_code == 200
+            assert b'{\"success\": true}' in response.data
 
-        mocked_response.ok = False
-        mocked_response.status_code = 500
-        response = app.test_client().post('/reserve/1234')
-        assert response.status_code == 500
-        assert b'{\"success\": false}' in response.data
+            mocked_response.ok = False
+            mocked_response.status_code = 500
+            response = app.test_client().post('/reserve/1234')
+            assert response.status_code == 500
+            assert b'{\"success\": false}' in response.data
